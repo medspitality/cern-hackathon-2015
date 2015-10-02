66e09e8fe49ee64104965e53337df3c3a25f22b4
Robert <roboter@abwesend.de>
2015-10-02 20:36:13 +0200

    setup node packages

---
+++ medspitality/setup-node-packages.bat
@@ -0,0 +1,38 @@
+@echo off
+
+::Ionic and cordova? ::cordova@5.1.1
+echo Install ionic (and thus cordova) globally, if not already present
+echo ---------------------------------------------------------------
+call npm list ionic@1.6.4 -g || npm install -g ionic@1.6.4
+if %ERRORLEVEL% equ 1 goto :fail
+
+::Bower?
+echo Install bower globally, if not already present
+echo ---------------------------------------------------------------
+call npm list bower -g || npm install -g bower
+if %ERRORLEVEL% equ 1 goto :fail
+
+::karma-cli?
+echo Install karma-cli globally, if not already present
+echo ---------------------------------------------------------------
+call npm list karma-cli@0.1.0 -g || npm install -g karma-cli@0.1.0
+if %ERRORLEVEL% equ 1 goto :fail
+
+::gulp?
+echo Install gulp globally, if not already present
+echo ---------------------------------------------------------------
+call npm list gulp@3.9.0 -g || npm install -g gulp@3.9.0
+if %ERRORLEVEL% equ 1 goto :fail
+
+::NPM package dependencies?
+echo Installing all needed node dependencies
+echo ---------------------------------------------------------------
+call npm install
+if %ERRORLEVEL% equ 1 goto :fail
+exit /b 0
+
+::Subroutine to exit with error code
+:fail
+echo An error occurred during execution. Exiting...
+exit /b 1
+:end
