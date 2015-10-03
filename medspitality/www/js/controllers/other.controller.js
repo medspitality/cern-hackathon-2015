8c1ef4153faceac66cb52bc07dd19b690fc213e0
Robert <roboter@abwesend.de>
2015-10-03 18:51:16 +0200

    splitted up controllers

---
+++ medspitality/www/js/controllers/other.controller.js
@@ -0,0 +1,44 @@
+angular.module('starter.controllers')
+
+.controller('WhoisWhoCtrl', function($scope) {
+    $scope.employees = [
+    {
+      id: 10,
+      image: 'person1.jpg',
+      name: 'Clara Healty'
+    },
+    {
+      id: 20,
+      image: 'person2.jpg',
+      name: 'Mike Headache'
+    },
+    {
+      id: 30,
+      image: 'person3.jpg',
+      name: 'Christina Happyfeet'
+    }
+  ];
+})
+
+.controller('SocialCtrl', function($scope) {
+  $scope.people = [
+    {
+      id: 1,
+      image: 'person1.jpg',
+      name: 'Clara Healty'
+    },
+    {
+      id: 2,
+      image: 'person2.jpg',
+      name: 'Mike Headache'
+    },
+    {
+      id: 3,
+      image: 'person3.jpg',
+      name: 'Christina Happyfeet'
+    }
+  ];
+})
+
+.controller('ChatCtrl', function($scope, $stateParams) {
+});
