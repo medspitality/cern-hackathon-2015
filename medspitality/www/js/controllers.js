a28ab768ee12f3bebc6abbedb115b43164b786ce
Fabricio PH <fabriph@gmail.com>
2015-10-03 11:06:25 +0200

    Feedback list.

--- medspitality/www/js/controllers.js
+++ medspitality/www/js/controllers.js
@@ -57,8 +57,18 @@ angular.module('starter.controllers', [])
 
 .controller('FeedbacklistCtrl', function($scope) {
   $scope.feedbacks = [
-    { title: 'Dr Ms Peralta', id: 1 },
-    { title: 'Clinic', id: 2 }
+    {
+      title: 'Dr Ms Peralta',
+      id: 1,
+      image: 'doctor_female_young.jpg',
+      description: 'Pediatric'
+    },
+    {
+      title: 'Barcelona Clinic',
+      id: 2,
+      image: 'logo_barcelona.png',
+      description: 'Villarroel 170, Barcelona, Spain'
+    }
   ];
 })
 
