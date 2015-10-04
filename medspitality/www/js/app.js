3b509a400aca5d144fccc88759f8ce99cee69238
asuccurro <a.succurro@gmail.com>
2015-10-04 15:41:35 +0200

    default timeline

--- medspitality/www/js/app.js
+++ medspitality/www/js/app.js
@@ -176,5 +176,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.factories','
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/profileslist');
+  $urlRouterProvider.otherwise('/app/timeline');
 });
